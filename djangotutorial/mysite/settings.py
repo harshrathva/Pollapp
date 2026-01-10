5232211a06a3d32f7ead1d645f1e5694260da935
harshrhrathva <harshrathva14@gmail.com>
2026-01-10 12:03:36 +0530

    [IMP] second commit migration and model creation

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Pollapp.apps.PollappConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
